2f080ed7e810341933b42ed885bd91c2e4568f5b
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2023-12-01 07:53:18 -0700

    solve the first day part one of aoc

--- 2023/day1/app/Main.hs
+++ 2023/day1/app/Main.hs
@@ -3,4 +3,11 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+   inputLines <- fetchLines "src/input.txt"
+   let digits = fetchDigits inputLines
+   let firsts = firstNumbers digits
+   let lasts = lastNumbers digits
+   let combinedValues = combineDigits firsts lasts
+   let sumValues = sum combinedValues
+   print sumValues
--- 2023/day1/src/Lib.hs
+++ 2023/day1/src/Lib.hs
@@ -1,6 +1,26 @@
 module Lib
-    ( someFunc
-    ) where
+  ( fetchLines
+  , fetchDigits
+  , combineDigits
+  , firstNumbers
+  , lastNumbers
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (isDigit)
+
+fetchLines :: String -> IO [String]
+fetchLines inputFile = lines <$> readFile inputFile
+
+fetchDigits :: [String] -> [String]
+fetchDigits = map $ filter isDigit
+
+combineDigits :: [Char] -> [Char] -> [Integer]
+combineDigits firsts lasts = zipWith combine firsts lasts
+  where
+    combine x y = read (x : [y])
+    
+firstNumbers :: [String] -> [Char]
+firstNumbers = map head 
+
+lastNumbers :: [String] -> [Char]
+lastNumbers = map (head . reverse) 
